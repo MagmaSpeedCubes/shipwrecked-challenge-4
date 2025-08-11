b0c691b6ba946338c87c5eb57f1ab3ef5315b374
MagmaSpeedCubes <110320002+MagmaSpeedCubes@users.noreply.github.com>
2025-08-11 01:30:32 -0400

    more functions

---
+++ Assets/Scripts/Shell.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Shell : MonoBehaviour
+{
+    [SerializeField] private Collider2D player;
+    [SerializeField] private AudioClip shellSound;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other == player)
+        {
+            AudioSource audioSource = GetComponent<AudioSource>();
+            audioSource.PlayOneShot(shellSound);
+            Destroy(gameObject);
+        }
+    }
+}
