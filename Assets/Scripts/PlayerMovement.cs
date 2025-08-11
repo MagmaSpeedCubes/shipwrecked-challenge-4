6cb9830de4d90a8717a241fc1b6eba07ced389a4
MagmaSpeedCubes <110320002+MagmaSpeedCubes@users.noreply.github.com>
2025-08-11 08:12:27 -0400

    finished levels

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,8 +12,10 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private bool isGrounded;
     private float moveInput;
+    private float moveInput2;
     private bool jumpInput;
     private bool jumpInput2;
+    private bool jumpInput3;
 
     void Start()
     {
@@ -25,16 +27,24 @@ public class PlayerMovement : MonoBehaviour
         moveInput = Keyboard.current.aKey.isPressed ? -1 :
                     Keyboard.current.dKey.isPressed ? 1 : 0;
 
+        moveInput2 = Keyboard.current.leftArrowKey.isPressed ? -1 :
+                     Keyboard.current.rightArrowKey.isPressed ? 1 : 0;
+
         
 
         jumpInput = Keyboard.current.spaceKey.wasPressedThisFrame;
         jumpInput2 = Keyboard.current.wKey.wasPressedThisFrame;
+        jumpInput3 = Keyboard.current.upArrowKey.wasPressedThisFrame;
 
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        if (moveInput2 != 0)
+        {
+            rb.linearVelocity = new Vector2(moveInput2 * moveSpeed, rb.linearVelocity.y);
+        }
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-        if ((jumpInput || jumpInput2) && isGrounded)
+        if ((jumpInput || jumpInput2 || jumpInput3) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
