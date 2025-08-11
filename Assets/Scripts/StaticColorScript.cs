8488101276fce33f1e8ad94c01181b94c7688a58
MagmaSpeedCubes <110320002+MagmaSpeedCubes@users.noreply.github.com>
2025-08-11 00:24:03 -0400

    debug mode + turtle

--- Assets/Scripts/StaticColorScript.cs
+++ Assets/Scripts/StaticColorScript.cs
@@ -5,12 +5,13 @@ public class StaticColorScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (!Constants.debugMode)
+        {
+           GetComponent<SpriteRenderer>().color = Color.white; // Change to desired static color 
+        }
+        // Set the color of the GameObject to a static color
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
